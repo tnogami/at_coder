c8d55106fc7cba26be807fca8d6b6376ec7534f2
tnogami <go2.f.add@gmail.com>
2022-02-22 20:53:34 +0900

    typical90 2022/02/22

--- problems/typical90/typical90_bh.py
+++ problems/typical90/typical90_bh.py
@@ -1,6 +1,29 @@
-from numpy import int128
+import bisect
 
+def Lis(A, ret):
+    L = [A[0]]
+    ret.append(1)
+    for a in A[1:]:
+        if L[-1] < a:
+            L.append(a)
+        else:
+            L[bisect.bisect_left(L,a)] = a
+        ret.append(len(L))
+    return ret
 
 N = int(input())
 A = list(map(int,input().split()))
 
+L1 = []
+L2 = []
+
+Lis(A, L1)
+Lis(A[::-1], L2)
+
+ans = 0
+for i in range(N):
+    idx1 = i
+    idx2 = N-1-i
+    ans = max(ans, L1[idx1]+L2[idx2]-1)
+
+print(ans)
